d3d5b95f328ade1789172cfd2c3a859b481a39f8
lucas <l.deutschmann@rubika-edu.com>
2020-12-02 15:26:23 +0100

    proto Eating Contest

--- WarioWare/Assets/MacroGame/TimedBehaviour.cs
+++ WarioWare/Assets/MacroGame/TimedBehaviour.cs
@@ -9,11 +9,10 @@ public class TimedBehaviour : MonoBehaviour
    [HideInInspector] public float bpm = 60;
     [HideInInspector] public Manager.difficulty currentDifficulty = 0;
 
-    private bool isInPlayableScene;
     public double timer;
     public double currentTime;
 
-    // tic increment every timed update, at 8 you must call the result
+    // Tick increments on every TimedUpdate(), at 8 you must call the result
     public int Tick
     {
         get;
@@ -24,7 +23,6 @@ public class TimedBehaviour : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name == "TestingScene")
         {
-            isInPlayableScene = true;
             bpm =(float) Manager.Instance.bpm;
             currentDifficulty = Manager.Instance.currentDifficulty;
             currentTime = AudioSettings.dspTime;
@@ -34,44 +32,23 @@ public class TimedBehaviour : MonoBehaviour
 
     public virtual void FixedUpdate()
     {
-        if (isInPlayableScene)
-            UpdatePlayableScene();
-        else
-            UpdateCustomScene();
-    }
-    #region SecuredUpdates
-    /// <summary>
-    /// Update on a custom scene of trio
-    /// </summary>
-    private void UpdateCustomScene()
-    {
-        timer = Time.deltaTime;
-        if (timer >= 60 / bpm)
-        {
-            timer = 0;
-            TimedUpdate();            
-        }
-    }
-    /// <summary>
-    /// Update on a scen with manager
-    /// </summary>
-    private void UpdatePlayableScene()
-    {
         timer = AudioSettings.dspTime - currentTime;
         if (timer >= 60 / bpm)
         {
+            Tick++;
             currentTime = AudioSettings.dspTime;
-            TimedUpdate();            
+            TimedUpdate();
+            Debug.Log(Tick);
         }
     }
-    #endregion
+  
 
     /// <summary>
-    /// TimedUdpate is call at each tic. Use this if you want your script to update with rithme
+    /// TimedUdpate is called at each tick. Use this if you want your script to update with rythme.
     /// </summary>
     public virtual void TimedUpdate()
     {
-        Tick++;
+        
     }
 
 }
---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/MiniGame/EatingContest/ScriptsEatingContest/PlatesManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// DEUTSCHMANN Lucas
+/// </summary>
+
+namespace TrioRadioRTL
+{
+    namespace EatingContest
+    {
+
+        public class PlatesManager : MonoBehaviour
+        {
+            bool isRotten;
+            public PlayerController playerController;
+
+            [Header("Graphs")]
+
+            public SpriteRenderer plate;
+            public Sprite goodPlateSprite;
+            public Sprite rottenPlateSprite;
+
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                
+                plate = gameObject.GetComponent<SpriteRenderer>();
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                isRotten = playerController.rottenPlate;
+
+                if (isRotten)
+                {
+                    plate.sprite = rottenPlateSprite;
+                }
+                else
+                {
+                    plate.sprite = goodPlateSprite;
+                }
+                
+                
+            }
+        }
+    }
+}
