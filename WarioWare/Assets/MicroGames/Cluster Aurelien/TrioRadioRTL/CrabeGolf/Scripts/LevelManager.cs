9e77c8f10c85e17f90744d936f2a17662fad995d
Théo <t.valet@rubika-edu.com>
2020-12-03 16:12:10 +0100

    Ajout du déplacement du crabe et du script du player

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/CrabeGolf/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RadioRTL
+{
+    /// <summary>
+    /// Théo Valet
+    /// </summary>
+}
+
+public class LevelManager : MonoBehaviour
+{
+
+}
