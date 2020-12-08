2023f7c2cce6c1c10f15b48f0a8b2558073f9468
Théo <t.valet@rubika-edu.com>
2020-12-08 19:30:27 +0100

    Le coup de club marche, les crabes normaux spawn
    La vie est belle putain

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/CrabeGolf/Scripts/CrabSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioName
+{
+    namespace MiniGameName
+    {
+        public class CrabSpawner : TimedBehaviour
+        {
+            public GameObject crab;
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+                if (Tick <= 4)
+                {
+                    Instantiate(crab,new Vector3(-8, -3, 0),Quaternion.identity);
+                }
+            }
+        }
+    }
+}
--- WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/CrabeGolf/Scripts/NormalCrabBehaviour.cs
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/CrabeGolf/Scripts/NormalCrabBehaviour.cs
@@ -13,12 +13,13 @@ namespace RadioRTL
     private Vector3 target;
     public float speed;
     private Vector3 position;
+    public bool isShot;
+    bool isFlying;
 
     void Start()
     {
         target = new Vector3(8, -3, 0);
         position = gameObject.transform.position;
-        speed = 1.0f;
     }
 
     void Update()
@@ -26,5 +27,20 @@ namespace RadioRTL
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target, step);
 
+        if (gameObject.transform.position == target)
+        {
+            Destroy(gameObject);
+        }
+
+        if (isShot)
+        {
+            if (!isFlying)
+            {
+                target = new Vector3(Random.Range(-8f, 8f), 3f, 0f);
+                isFlying = true;
+            }
+
+            speed = 20;
+        }
     }
 }
