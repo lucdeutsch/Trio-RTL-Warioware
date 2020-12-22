13aade51990e9083ae698f003da1e0fd57e0ee03
Théo <t.valet@rubika-edu.com>
2020-12-22 16:10:46 +0100

    Création du Sound Manager avec les VFX et les musiques
    ça marche pas étant donné que j'ai un problème de null référence je ne sait où

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/CrabeGolf/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using Testing;
+
+
+namespace RadioRTL
+{
+    /// <summary>
+    /// Théo Valet
+    /// </summary>
+
+    namespace CrabGolf
+    {
+        public class AudioManager : MonoBehaviour
+        {
+            public Sounds[] sounds;
+
+            private void Awake()    
+            {
+                foreach (Sounds s in sounds)
+                {
+                    s.source = gameObject.AddComponent<AudioSource>();
+                    s.source.clip = s.clip;
+
+                    s.source.volume = s.volume;
+                    s.source.pitch = s.pitch;
+                    s.source.loop = s.loop;
+                }
+            }
+            public void Play (string name)
+            {
+                Sounds s = Array.Find(sounds, sound => sound.name == name);
+                s.source.Play();
+            }
+        }
+    }
+}
+
