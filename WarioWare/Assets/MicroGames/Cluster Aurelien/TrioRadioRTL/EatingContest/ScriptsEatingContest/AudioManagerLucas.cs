1a2815e7692fe1d4b6dfb60b42acf766f267cbe4
lucas <l.deutschmann@rubika-edu.com>
2021-01-15 18:47:27 +0100

    audio fix

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/EatingContest/ScriptsEatingContest/AudioManagerLucas.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using Testing;
+
+
+namespace RadioRTL
+{
+    /// <summary>
+    /// DEUTSCHMANN Lucas
+    /// </summary>
+
+    namespace EatingContest
+    {
+        public class AudioManagerLucas : MonoBehaviour
+        {
+            public SoundsLucas[] sounds;
+            public AudioSource gestionSon;
+
+            private void Awake()
+            {
+                foreach (SoundsLucas s in sounds)
+                {
+                    s.source = gameObject.AddComponent<AudioSource>();
+                    s.source.clip = s.clip;
+
+                    s.source.volume = s.volume;
+                    s.source.pitch = s.pitch;
+                    s.source.loop = s.loop;
+                }
+            }
+            public void Play(string name)
+            {
+                SoundsLucas s = Array.Find(sounds, sound => sound.name == name);
+                s.source.Play();
+                gestionSon.clip = s.source.clip;
+            }
+        }
+    }
+}
+
